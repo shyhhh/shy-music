f22080c45f208eecc33ace72e43072125df677f0
shyhhh <hhhtheshy@gmail.com>
2023-03-16 00:17:44 +0800

    配置别名

--- craco.config.js
+++ craco.config.js
@@ -1,5 +1,13 @@
+const path = require('node:path')
 const CracoLessPlugin = require('craco-less')
 
+const resolve = (dir) => path.resolve(__dirname, dir)
+
 module.exports = {
-  plugins: [{ plugin: CracoLessPlugin }]
+  plugins: [{ plugin: CracoLessPlugin }],
+  webpack: {
+    alias: {
+      '@': resolve('src')
+    }
+  }
 }
