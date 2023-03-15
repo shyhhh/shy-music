2ba3b31574bf28e8d1c7ef0a4740af4965c1ffe4
shyhhh <hhhtheshy@gmail.com>
2023-03-15 22:51:56 +0800

    css 样式重置

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import 'normalize.css'
+import './assets/css/index.less'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
