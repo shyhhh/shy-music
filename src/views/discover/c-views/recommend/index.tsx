47b1164fb53348a5b9c3570efbb1d533603effed
shyhhh <hhhtheshy@gmail.com>
2023-03-18 01:23:10 +0800

    axios 请求测试

--- src/views/discover/c-views/recommend/index.tsx
+++ src/views/discover/c-views/recommend/index.tsx
@@ -1,10 +1,31 @@
-import { memo } from 'react'
+import { memo, useEffect, useState } from 'react'
+import hyRequest from '@/service'
 
+export interface IBannerData {
+  imageUrl: string
+  targetId: number
+  targetType: number
+  titleColor: string
+  typeTitle: string
+  url: string
+  exclusive: boolean
+  scm: string
+  bannerBizType: string
+}
 const Recommend: React.FC = memo(() => {
+  const [banners, setBanners] = useState<IBannerData[]>([])
+  // 测试网络请求
+  useEffect(() => {
+    hyRequest.get({
+      url: '/banner'
+    }).then((res) => {
+      setBanners(res.banners)
+    })
+  }, [])
   return (
     <div>
-      recommend
-    </div>
+      {banners.map(item => <div key={item.imageUrl} >{item.imageUrl}</div>)}
+    </div >
   )
 })
 
