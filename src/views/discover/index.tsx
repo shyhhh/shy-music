588e0df3add7714b20bb915137c26d42dca93630
shyhhh <hhhtheshy@gmail.com>
2023-03-19 16:59:52 +0800

    discover 顶部导航菜单

--- src/views/discover/index.tsx
+++ src/views/discover/index.tsx
@@ -1,15 +1,11 @@
 import { Suspense, memo } from 'react'
-import { Link, Outlet } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
+import NavBar from './c-cpns/nav-bar'
 
 const Discover: React.FC = memo(() => {
   return (
     <>
-      <Link to={'/discover/recommend'}>推荐</Link>
-      <Link to={'/discover/ranking'}>排行榜</Link>
-      <Link to={'/discover/songs'}>歌单</Link>
-      <Link to={'/discover/djradio'}>主播电台</Link>
-      <Link to={'/discover/artist'}>歌手</Link>
-      <Link to={'/discover/album'}>新碟上架</Link>
+      <NavBar/>
       <Suspense fallback=''>
         <Outlet />
       </Suspense>
